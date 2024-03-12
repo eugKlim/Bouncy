fb5f6a9e27ed45983344bc783f32d5a28afe8931
Eugene <eugKlim1@gmail.com>
2024-03-12 22:51:46 +0300

    Create block testimonials and block talk & create adaptive for him

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,17 @@
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
 
-const swiper = new Swiper('.team-swiper', {
+const swiper1 = new Swiper('.team-swiper', {
+  modules: [Navigation, Pagination],
+  loop: true,
+  
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+const swiper2 = new Swiper('.testimonials-swiper', {
   modules: [Navigation, Pagination],
   loop: true,
   
