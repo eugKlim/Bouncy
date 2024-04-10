3f166f2ac10486ab7ad3af35c73a34f27b45aefe
Eugene Klimenko <eugklim1@gmail.com>
2024-04-10 13:59:06 +0300

    the assembly has been optimized to work with js

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ const imagemin = require('gulp-imagemin');
 const newer = require('gulp-newer');
 const gulpIf = require('gulp-if');
 const path = require('path');
-var plumber = require('gulp-plumber'); 
+var plumber = require('gulp-plumber');
 const fs = require('fs');
 const fileInclude = require('gulp-file-include');
 const webpackStream = require('webpack-stream');
@@ -22,8 +22,6 @@ const sourcemaps = require('gulp-sourcemaps');
 const ghPages = require('gh-pages');
 const { promisify } = require('util');
 const publishToGitHub = promisify(ghPages.publish);
-
-
 
 function convertTTFtoWOFF() {
   return src(['src/fonts/*.ttf', 'src/fonts/*.otf'])
@@ -44,41 +42,138 @@ function convertTTFtoWOFF2() {
 function fontsStyle() {
   let fontsFile = 'src/scss/fonts.scss';
   if (!fs.existsSync('src/fonts') || fs.readdirSync('src/fonts').length === 0) {
-    console.log("Отсутствуют шрифты в каталоге src/fonts. Задача fontsStyle не будет выполнена.");
+    console.log('Отсутствуют шрифты в каталоге src/fonts. Задача fontsStyle не будет выполнена.');
     return src('src');
   }
   let fontsFiles = fs.readdirSync('dist/fonts');
-    if (fontsFiles && !fs.existsSync(fontsFile)) {
-      fs.writeFileSync(fontsFile, '');
-      let newFileOnly;
-      for (var i = 0; i < fontsFiles.length; i++) {
-        let fontFileName = fontsFiles[i].split('.')[0];
-        if (newFileOnly !== fontFileName) {
-          let fontName = fontFileName.split('-')[0] || fontFileName;
-          let fontWeight = fontFileName.split('-')[1] || fontFileName;
-          fs.appendFileSync(fontsFile, `@font-face{\n\tfont-family: '${fontName} ${fontWeight}';\n\tfont-display: swap;\n\tsrc: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");\n\tfont-weight: ${fontWeight};\n\tfont-style: normal;\n}\r\n`);
-          newFileOnly = fontFileName;
-        }
+  if (fontsFiles && !fs.existsSync(fontsFile)) {
+    fs.writeFileSync(fontsFile, '');
+    let newFileOnly;
+    for (var i = 0; i < fontsFiles.length; i++) {
+      let fontFileName = fontsFiles[i].split('.')[0];
+      if (newFileOnly !== fontFileName) {
+        let fontName = fontFileName.split('-')[0] || fontFileName;
+        let fontWeight = fontFileName.split('-')[1] || fontFileName;
+        fs.appendFileSync(fontsFile, `@font-face{\n\tfont-family: '${fontName} ${fontWeight}';\n\tfont-display: swap;\n\tsrc: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");\n\tfont-weight: ${fontWeight};\n\tfont-style: normal;\n}\r\n`);
+        newFileOnly = fontFileName;
       }
-    } else {
-      console.log("Файл scss/fonts.scss уже существует. Для обновления файла нужно его удалить!");
-    }  
-    return src('src');
+    }
+  } else {
+    console.log('Файл scss/fonts.scss уже существует. Для обновления файла нужно его удалить!');
+  }
+  return src('src');
 }
 function styles() {
-  return (
-    src('src/scss/*.scss')
-      .pipe(autoprefixer({ overrideBrowserslist: ['last 20 version'] }))
-      .pipe(concat('style.min.css'))
-      .pipe(scss({ outputStyle: 'compressed' }))
-      .pipe(dest('dist/css'))
-      .pipe(browserSync.stream())
-  );
+  return src('src/scss/*.scss')
+    .pipe(autoprefixer({ overrideBrowserslist: ['last 20 version'] }))
+    .pipe(concat('style.min.css'))
+    .pipe(scss({ outputStyle: 'compressed' }))
+    .pipe(dest('dist/css'))
+    .pipe(browserSync.stream());
 }
 function scripts() {
   return src(['src/js/**/*.js', '!src/js/codeToBuild/**'])
     .pipe(
       webpackStream({
+        mode: 'development',
+        output: {
+          filename: 'main.min.js',
+        },
+        module: {
+          rules: [
+            {
+              test: /\.(?:js|mjs|cjs)$/,
+              exclude: /node_modules|bower_components/,
+            },
+          ],
+        },
+      })
+    )
+
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(dest('dist/js'))
+    .pipe(browserSync.stream());
+}
+function html() {
+  return src(['src/*.html', '!src/htmlBlocks/**/*.*']).pipe(dest('dist')).pipe(browserSync.stream());
+}
+function cleanDist() {
+  return src(['dist']).pipe(clean());
+}
+function imageOptimization() {
+  return src(['src/media/image/**/*.*', '!src/media/image/**/*.svg', '!src/media/image/favicon/**/*.*'])
+    .pipe(avif({ quality: 50 }))
+    .pipe(src(['src/media/image/**/*.*', '!src/media/image/favicon/**/*.*']))
+    .pipe(webp())
+    .pipe(src('src/media/image/**/*.*'))
+    .pipe(imagemin())
+    .pipe(dest('dist/media/image'));
+}
+function files() {
+  return src(['src/files/**/*.*']).pipe(dest('dist/files'));
+}
+function mediaFiles() {
+  return src('src/media/**/*.*')
+    .pipe(
+      gulpIf(
+        (file) => {
+          const ext = path.extname(file.path).toLowerCase();
+          if (['.jpg', '.png', '.svg'].includes(ext)) {
+            return true;
+          } else if (['.mp4', '.webm'].includes(ext)) {
+            return true;
+          } else if (ext === '.gif') {
+            return true;
+          } else if (ext === '.mp3') {
+            return true;
+          } else if (['.webmanifest', '.xml', '.ico', '.json'].includes(ext)) {
+            return true;
+          }
+          return false;
+        },
+        dest((file) => {
+          const ext = path.extname(file.path).toLowerCase();
+          if (['.jpg', '.png', '.svg'].includes(ext)) {
+            return 'dist/media';
+          } else if (['.mp4', '.webm'].includes(ext)) {
+            return 'dist/media';
+          } else if (ext === '.gif') {
+            return 'dist/media';
+          } else if (ext === '.mp3') {
+            return 'dist/media';
+          } else if (['.webmanifest', '.xml', '.ico', '.json'].includes(ext)) {
+            return 'dist/media';
+          }
+        })
+      )
+    )
+    .pipe(browserSync.stream());
+}
+function includeFile() {
+  return src('src/*.html')
+    .pipe(
+      fileInclude({
+        prefix: '@@',
+        basepath: 'src/htmlBlocks',
+      })
+    )
+    .pipe(dest('dist'))
+    .pipe(browserSync.stream());
+}
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: 'dist',
+    },
+  });
+}
+function addCodeToProd() {
+  return src(['src/js/**/*.js'])
+    .pipe(
+      webpackStream({
+        mode: 'production',
         output: {
           filename: 'main.min.js',
         },
@@ -90,9 +185,7 @@ function scripts() {
               use: {
                 loader: 'babel-loader',
                 options: {
-                  presets: [
-                    ['@babel/preset-env', { targets: 'defaults' }],
-                  ],
+                  presets: [['@babel/preset-env', { targets: 'defaults' }]],
                 },
               },
             },
@@ -101,113 +194,9 @@ function scripts() {
       })
     )
 
-    // .pipe(sourcemaps.init())
     .pipe(uglify())
-    // .pipe(sourcemaps.write())
     .pipe(dest('dist/js'))
     .pipe(browserSync.stream());
-}
-function html() {
-  return src(['src/*.html', '!src/htmlBlocks/**/*.*'])
-  .pipe(dest('dist'))
-  .pipe(browserSync.stream());
-}
-function cleanDist() {
-  return src(['dist'])
-  .pipe(clean())
-}
-function imageOptimization() {
-  return src(['src/media/image/**/*.*', '!src/media/image/**/*.svg', '!src/media/image/favicon/**/*.*'])
-  .pipe(avif({quality: 50}))
-  .pipe(src(['src/media/image/**/*.*', '!src/media/image/favicon/**/*.*']))
-  .pipe(webp())
-  .pipe(src('src/media/image/**/*.*'))
-  .pipe(imagemin())
-  .pipe(dest('dist/media/image'))
-}
-function files() {
-  return src(['src/files/**/*.*'])
-  .pipe(dest('dist/files'))
-}
-function mediaFiles() {
-  return src('src/media/**/*.*')
-    .pipe(gulpIf(file => {
-      const ext = path.extname(file.path).toLowerCase();
-      if (['.jpg', '.png', '.svg'].includes(ext)) {
-        return true;
-      } else if (['.mp4', '.webm'].includes(ext)) {
-        return true;
-      } else if (ext === '.gif') {
-        return true;
-      } else if (ext === '.mp3') {
-        return true;
-      } else if (['.webmanifest', '.xml', '.ico', '.json'].includes(ext)) {
-        return true;
-      }
-      return false;
-    }, dest(file => {
-      const ext = path.extname(file.path).toLowerCase();
-      if (['.jpg', '.png', '.svg'].includes(ext)) {
-        return 'dist/media';
-      } else if (['.mp4', '.webm'].includes(ext)) {
-        return 'dist/media';
-      } else if (ext === '.gif') {
-        return 'dist/media';
-      } else if (ext === '.mp3') {
-        return 'dist/media';
-      } else if (['.webmanifest', '.xml', '.ico', '.json'].includes(ext)) {
-        return 'dist/media';
-      }
-    })))
-    .pipe(browserSync.stream());
-}
-function includeFile() {
-  return src('src/*.html') 
-  .pipe(fileInclude({
-    prefix: '@@',
-    basepath: 'src/htmlBlocks'
-  }))
-  .pipe(dest('dist'))
-  .pipe(browserSync.stream())
-}
-function browsersync() {
-  browserSync.init({
-    server: {
-      baseDir: 'dist',
-    },
-  });
-}
-function addCodeToProd() {
-  return src(['src/js/**/*.js'])
-  .pipe(
-    webpackStream({
-      output: {
-        filename: 'main.min.js',
-      },
-      module: {
-        rules: [
-          {
-            test: /\.(?:js|mjs|cjs)$/,
-            exclude: /node_modules|bower_components/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: [
-                  ['@babel/preset-env', { targets: 'defaults' }],
-                ],
-              },
-            },
-          },
-        ],
-      },
-    })
-  )
-
-  // .pipe(sourcemaps.init())
-  .pipe(uglify())
-  // .pipe(sourcemaps.write())
-  .pipe(dest('dist/js'))
-  .pipe(browserSync.stream());
 }
 function watching() {
   watch(['src/scss/vars.scss'], styles);
@@ -215,16 +204,16 @@ function watching() {
   watch(['src/scss/mediaQueries.scss'], styles);
   watch(['src/js/main.js', 'src/js/modules/**/*.js'], scripts);
   watch('src/media/**/*.*', mediaFiles);
-  watch('src/htmlBlocks/**/*.html', includeFile, html );
-  watch(['src/*.html'],  includeFile, html).on('change', browserSync.reload);
+  watch('src/htmlBlocks/**/*.html', includeFile, html);
+  watch(['src/*.html'], includeFile, html).on('change', browserSync.reload);
 }
 
 async function pushToGithubGhPages() {
   try {
-      await publishToGitHub('dist', { branch: 'gh-pages' });
-      console.log('Deployment to GitHub Pages successful');
+    await publishToGitHub('dist', { branch: 'gh-pages' });
+    console.log('Deployment to GitHub Pages successful');
   } catch (err) {
-      console.error(err);
+    console.error(err);
   }
 }
 
@@ -241,8 +230,8 @@ exports.convertTTFtoWOFF = convertTTFtoWOFF;
 exports.convertTTFtoWOFF2 = convertTTFtoWOFF2;
 exports.includeFile = includeFile;
 
-exports.build = series(cleanDist, html, includeFile, mediaFiles, files,  imageOptimization, series( convertTTFtoWOFF, convertTTFtoWOFF2,  fontsStyle, styles),  scripts, addCodeToProd);
+exports.build = series(cleanDist, html, includeFile, mediaFiles, files, imageOptimization, series(convertTTFtoWOFF, convertTTFtoWOFF2, fontsStyle, styles), scripts, addCodeToProd);
 
-exports.default = series( html, includeFile, mediaFiles, files, convertTTFtoWOFF, convertTTFtoWOFF2,  fontsStyle, styles, scripts, parallel(browsersync, watching));
+exports.default = series(html, includeFile, mediaFiles, files, convertTTFtoWOFF, convertTTFtoWOFF2, fontsStyle, styles, scripts, parallel(browsersync, watching));
 
-exports.deploy = series(cleanDist, html, includeFile, mediaFiles, files,  imageOptimization, series( convertTTFtoWOFF, convertTTFtoWOFF2,  fontsStyle, styles),  scripts, addCodeToProd, pushToGithubGhPages);
+exports.deploy = series(cleanDist, html, includeFile, mediaFiles, files, imageOptimization, series(convertTTFtoWOFF, convertTTFtoWOFF2, fontsStyle, styles), scripts, addCodeToProd, pushToGithubGhPages);
